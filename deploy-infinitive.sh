eb2b196f05e6c2f4c7bcd7c80a59a236263a55b8
Charlie Heitzig <mail@heitzig.org>
2026-02-04 18:21:17 -0600

    deploy script for pi

---
+++ deploy-infinitive.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# ~/dev/homebridge-infinitive/deploy-infinitive.sh
+cd ~/dev/homebridge-infinitive
+git pull
+npm install
+npm run build
+sudo rm -rf /var/lib/homebridge/node_modules/homebridge-infinitive
+sudo cp -r ~/dev/homebridge-infinitive /var/lib/homebridge/node_modules/
+sudo chown -R homebridge:homebridge /var/lib/homebridge/node_modules/homebridge-infinitive
+sudo hb-service restart
